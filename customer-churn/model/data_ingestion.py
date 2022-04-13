2432f2cc5a1e335c5a625bb1c2ce364523d24cb8
Ayush Singh <neweraofficial488@gmail.com>
2022-04-14 00:59:45 +0530

    added data ingestion

---
+++ customer-churn/model/data_ingestion.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+import pandas as pd
+
+
+class DataIngestion:
+    """Class for data Ingestion"""
+
+    def __init__(self) -> None:
+        self.path = "../data"
+
+    def read_data(self) -> pd.DataFrame:
+        """Reads data from csv files and merge the csv files into one dataframe"""
+        try:
+            customer_churn_df = pd.read_csv(os.path.join(self.path, "customer-churn-data.csv"))
+            return customer_churn_df
+        except Exception as e:
+            logging.error(e)
