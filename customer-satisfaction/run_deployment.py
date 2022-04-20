dc2f1e8c2b450b35a9c5e0e21094abfc7ca4cf05
Ayush Singh <neweraofficial488@gmail.com>
2022-04-20 15:57:13 +0530

    added mlflow deployment updates

--- customer-satisfaction/run_deployment.py
+++ customer-satisfaction/run_deployment.py
@@ -16,10 +16,7 @@ from steps.clean_data import clean_data
 from steps.evaluation import evaluation
 from steps.ingest_data import ingest_data
 from steps.model_train import train_model
-from zenml.environment import Environment
-from zenml.integrations.mlflow.mlflow_environment import (
-    MLFLOW_ENVIRONMENT_NAME,
-)
+from zenml.integrations.mlflow.mlflow_environment import global_mlflow_env
 from zenml.integrations.mlflow.steps import MLFlowDeployerConfig
 from zenml.services import load_last_service_from_step
 
@@ -75,15 +72,14 @@ def main(min_accuracy: float, stop_service: bool):
     )
     inference.run()
 
-    mlflow_env = Environment()[MLFLOW_ENVIRONMENT_NAME]
-    print(
-        "You can run:\n "
-        f"[italic green]    mlflow ui --backend-store-uri {mlflow_env.tracking_uri}[/italic green]\n"
-        "...to inspect your experiment runs within the MLflow UI.\n"
-        "You can find your runs tracked within the `mlflow_example_pipeline`"
-        "experiment. There you'll also be able to compare two or more runs.\n\n"
-    )
-
+    with global_mlflow_env() as mlflow_env:
+        print(
+            "You can run:\n "
+            f"[italic green]    mlflow ui --backend-store-uri {mlflow_env.tracking_uri}[/italic green]\n"
+            "...to inspect your experiment runs within the MLflow UI.\n"
+            "You can find your runs tracked within the `mlflow_example_pipeline`"
+            "experiment. There you'll also be able to compare two or more runs.\n\n"
+        )
     service = load_last_service_from_step(
         pipeline_name="continuous_deployment_pipeline",
         step_name="model_deployer",
@@ -93,7 +89,7 @@ def main(min_accuracy: float, stop_service: bool):
         print(
             f"The MLflow prediction server is running locally as a daemon process "
             f"and accepts inference requests at:\n"
-            f"    {service.prediction_uri}\n"
+            f"    {service.prediction_url}\n"
             f"To stop the service, re-run the same command and supply the "
             f"`--stop-service` argument."
         )
