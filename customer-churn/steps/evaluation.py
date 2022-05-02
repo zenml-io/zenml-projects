7b6551921e47b582086db1302fc23423ff2d3ae8
AyushSingh <ayush.ext@zenml.io>
2022-05-02 21:10:31 +0530

    bug fixes in steps

--- customer-churn/steps/evaluation.py
+++ customer-churn/steps/evaluation.py
@@ -1,7 +1,5 @@
-import mlflow
 import pandas as pd
 from sklearn.base import ClassifierMixin
-from zenml.integrations.mlflow.mlflow_step_decorator import enable_mlflow
 from zenml.logger import get_logger
 from zenml.steps import Output, step
 
@@ -10,7 +8,6 @@ from .src.evaluator import Evaluation
 logger = get_logger(__name__)
 
 
-@enable_mlflow
 @step
 def evaluation(model: ClassifierMixin, test: pd.DataFrame) -> Output(accuracy=float):
     """
@@ -27,7 +24,6 @@ def evaluation(model: ClassifierMixin, test: pd.DataFrame) -> Output(accuracy=fl
         prediction = model.predict(X)
         evaluation = Evaluation(y, prediction)
         accuracy = evaluation.get_accuracy()
-        mlflow.log_metric("accuracy", accuracy)
         return accuracy
     except Exception as e:
         logger.error(e)
--- customer-churn/steps/trainer.py
+++ customer-churn/steps/trainer.py
@@ -1,8 +1,6 @@
-import mlflow
 import pandas as pd
 from sklearn.base import ClassifierMixin
 from sklearn.model_selection import train_test_split
-from zenml.integrations.mlflow.mlflow_step_decorator import enable_mlflow
 from zenml.logger import get_logger
 from zenml.steps import BaseStepConfig, Output, step
 
@@ -14,11 +12,10 @@ logger = get_logger(__name__)
 class ModelNameConfig(BaseStepConfig):
     """Model Configurations"""
 
-    model_name: str = "lightgbm"
+    model_name: str = "randomforest"
     fine_tuning: bool = False
 
 
-@enable_mlflow
 @step
 def model_trainer(train: pd.DataFrame, config: ModelNameConfig) -> Output(model=ClassifierMixin):
     """Trains a specified model."""
@@ -29,15 +26,12 @@ def model_trainer(train: pd.DataFrame, config: ModelNameConfig) -> Output(model=
         tree_based_model = TreeBasedModels(x_train, y_train, x_test, y_test)
 
         if config.model_name == "lightgbm":
-            mlflow.lightgbm.autolog()
             lgm_model = tree_based_model.lightgbm_trainer(fine_tuning=config.fine_tuning)
             return lgm_model
         elif config.model_name == "randomforest":
-            mlflow.sklearn.autolog()
             rf_model = tree_based_model.random_forest_trainer(fine_tuning=config.fine_tuning)
             return rf_model
         elif config.model_name == "xgboost":
-            mlflow.xgboost.autolog()
             xgb_model = tree_based_model.xgboost_trainer(fine_tuning=config.fine_tuning)
             return xgb_model
         else:
