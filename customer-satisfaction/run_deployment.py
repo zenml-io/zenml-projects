9a115ed6a7071d03cb3ba54ff245320a9ad166fa
Ayush Singh <81796368+ayush714@users.noreply.github.com>
2022-05-16 15:45:04 +0530

    Update to new Version  (#31)
    * Update run_deployment.py
    
    * added requirements
    
    * upgrade mlflow tracking to zenml0.7.3
    
    * upgrade to zenml 0.7.3
    
    * added streamlit functionalities
    
    * Update customer-satisfaction/streamlit_app.py
    
    Co-authored-by: Alexej Penner <thealexejpenner@gmail.com>
    
    Co-authored-by: Alexej Penner <thealexejpenner@gmail.com>

--- customer-satisfaction/run_deployment.py
+++ customer-satisfaction/run_deployment.py
@@ -16,7 +16,7 @@ from steps.clean_data import clean_data
 from steps.evaluation import evaluation
 from steps.ingest_data import ingest_data
 from steps.model_train import train_model
-from zenml.integrations.mlflow.mlflow_environment import global_mlflow_env
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
 from zenml.integrations.mlflow.steps import MLFlowDeployerConfig
 from zenml.services import load_last_service_from_step
 
@@ -33,7 +33,7 @@ from zenml.services import load_last_service_from_step
     default=False,
     help="Stop the prediction service when done",
 )
-def main(min_accuracy: float, stop_service: bool):
+def run_main(min_accuracy: float, stop_service: bool):
 
     """Run the mlflow example pipeline"""
     if stop_service:
@@ -72,14 +72,14 @@ def main(min_accuracy: float, stop_service: bool):
     )
     inference.run()
 
-    with global_mlflow_env() as mlflow_env:
-        print(
-            "You can run:\n "
-            f"[italic green]    mlflow ui --backend-store-uri {mlflow_env.tracking_uri}[/italic green]\n"
-            "...to inspect your experiment runs within the MLflow UI.\n"
-            "You can find your runs tracked within the `mlflow_example_pipeline`"
-            "experiment. There you'll also be able to compare two or more runs.\n\n"
-        )
+    print(
+        "Now run \n "
+        f"    mlflow ui --backend-store-uri {get_tracking_uri()}\n"
+        "To inspect your experiment runs within the mlflow UI.\n"
+        "You can find your runs tracked within the `mlflow_example_pipeline`"
+        "experiment. Here you'll also be able to compare the two runs.)"
+    )
+
     service = load_last_service_from_step(
         pipeline_name="continuous_deployment_pipeline",
         step_name="model_deployer",
@@ -95,5 +95,5 @@ def main(min_accuracy: float, stop_service: bool):
         )
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
--- customer-satisfaction/run_pipeline.py
+++ customer-satisfaction/run_pipeline.py
@@ -4,7 +4,7 @@ from steps.clean_data import clean_data
 from steps.evaluation import evaluation
 from steps.ingest_data import ingest_data
 from steps.model_train import train_model
-from zenml.integrations.mlflow.mlflow_environment import global_mlflow_env
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
 
 
 def run_training():
@@ -21,11 +21,10 @@ def run_training():
 if __name__ == "__main__":
     run_training()
 
-    with global_mlflow_env() as mlflow_env:
-        print(
-            "Now run \n "
-            f"    mlflow ui --backend-store-uri {mlflow_env.tracking_uri}\n"
-            "To inspect your experiment runs within the mlflow ui.\n"
-            "You can find your runs tracked within the `mlflow_example_pipeline`"
-            "experiment. Here you'll also be able to compare the two runs.)"
-        )
+    print(
+        "Now run \n "
+        f"    mlflow ui --backend-store-uri {get_tracking_uri()}\n"
+        "To inspect your experiment runs within the mlflow UI.\n"
+        "You can find your runs tracked within the `mlflow_example_pipeline`"
+        "experiment. Here you'll also be able to compare the two runs.)"
+    )
