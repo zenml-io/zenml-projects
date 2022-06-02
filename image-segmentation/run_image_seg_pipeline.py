70695538ebf6588206a19cf97e1334fd9e22d46d
AyushSingh <ayush.ext@zenml.io>
2022-06-02 18:19:56 +0530

    added the pipeline but not running correctly

---
+++ image-segmentation/run_image_seg_pipeline.py
@@ -0,0 +1,21 @@
+from pipelines.run_image_seg_pipeline import image_segmentation_pipeline
+from steps.data_steps import (
+    apply_augmentations,
+    create_stratified_fold,
+    prepare_dataloaders,
+    prepare_df,
+)
+
+
+def run_img_seg_pipe():
+    """
+    TODO:
+    """
+    image_seg_pipe = image_segmentation_pipeline(
+        prepare_df(), create_stratified_fold(), apply_augmentations(), prepare_dataloaders()
+    )
+    image_seg_pipe.run()
+
+
+if __name__ == "__main__":
+    run_img_seg_pipe()
