9b70e642bbe87a48286da29a2352b0b48f3e8a93
AyushSingh <ayush.ext@zenml.io>
2022-06-02 22:15:06 +0530

    removed augmentation from pipeline

--- image-segmentation/pipelines/run_image_seg_pipeline.py
+++ image-segmentation/pipelines/run_image_seg_pipeline.py
@@ -2,13 +2,10 @@ from zenml.pipelines import pipeline
 
 
 @pipeline(enable_cache=False)
-def image_segmentation_pipeline(
-    prepare_df, create_stratified_fold, apply_augmentations, prepare_dataloaders
-):
+def image_segmentation_pipeline(prepare_df, create_stratified_fold, prepare_dataloaders):
     """
     TODO
     """
     df = prepare_df()
     fold_dfs = create_stratified_fold(df)
-    data_transforms = apply_augmentations()
     train_loader, valid_loader = prepare_dataloaders(fold_dfs)
