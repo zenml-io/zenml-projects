ce1ac61161cef24021898dff3f555ea8a52097ba
AyushSingh <ayush.ext@zenml.io>
2022-06-02 18:20:30 +0530

    modified the pipelines

--- image-segmentation/pipelines/run_image_seg_pipeline.py
+++ image-segmentation/pipelines/run_image_seg_pipeline.py
@@ -1,8 +1,14 @@
-from asyncio.windows_utils import pipe
-
 from zenml.pipelines import pipeline
 
 
 @pipeline(enable_cache=False)
-def image_segmentation_pipeline(prepare_df, create_stratified_fold):
-    pass
+def image_segmentation_pipeline(
+    prepare_df, create_stratified_fold, apply_augmentations, prepare_dataloaders
+):
+    """
+    TODO
+    """
+    df = prepare_df()
+    fold_dfs = create_stratified_fold(df)
+    data_transforms = apply_augmentations()
+    train_loader, valid_loader = prepare_dataloaders(fold_dfs)
