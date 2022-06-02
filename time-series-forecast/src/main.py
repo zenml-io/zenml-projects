67203b6f10ba044791ca2ef9ba5456daf4ef74ce
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-06-02 13:31:39 +0200

    Delete not used packages from main

--- time-series-forecast/src/main.py
+++ time-series-forecast/src/main.py
@@ -1,13 +1,4 @@
-from google.oauth2 import service_account
 from zenml.pipelines import pipeline
-from zenml.steps import step, Output, BaseStepConfig
-from zenml.repository import Repository
-import pandas_gbq
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score
 from steps.importer import bigquery_importer
 from steps.preparator import preparator
 from steps.transformer import transformer
@@ -15,21 +6,20 @@ from steps.trainer import trainer
 from steps.evaluator import evaluator
 
 @pipeline
-def iris_pipeline(
+def time_series_pipeline(
     bigquery_importer,
     preparator,
     transformer,
     trainer,
     evaluator,
 ):
-    """Links all the steps together in a pipeline"""
-    data = bigquery_importer() 
-    prepared_data = preparator(data=data) #, X_test, y_train, y_test = preparator(data=data)
-    X_train, X_test, y_train, y_test = transformer(data=prepared_data) #splitter(data=transformed_data)
+    data = bigquery_importer()
+    prepared_data = preparator(data=data)
+    X_train, X_test, y_train, y_test = transformer(data=prepared_data)
     model = trainer(X_train=X_train,y_train=y_train)
     evaluator(X_test=X_test, y_test=y_test, model=model)
 
-pipeline = iris_pipeline(
+pipeline = time_series_pipeline(
     bigquery_importer=bigquery_importer(),
     preparator=preparator(),
     transformer=transformer(),
@@ -37,4 +27,5 @@ pipeline = iris_pipeline(
     evaluator=evaluator(),
 )
 
-pipeline.run()
+if __name__ == "__main__":
+    pipeline.run()
