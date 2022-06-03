4e72e12a77b28e9f068513b2609393551f7af2c9
AyushSingh <ayush.ext@zenml.io>
2022-06-03 14:11:59 +0530

    added model train in pipeline

--- image-segmentation/pipelines/run_image_seg_pipeline.py
+++ image-segmentation/pipelines/run_image_seg_pipeline.py
@@ -2,10 +2,18 @@ from zenml.pipelines import pipeline
 
 
 @pipeline(enable_cache=False)
-def image_segmentation_pipeline(prepare_df, create_stratified_fold, prepare_dataloaders):
+def image_segmentation_pipeline(
+    prepare_df,
+    create_stratified_fold,
+    prepare_dataloaders,
+    initiate_model_and_optimizer,
+    train_model,
+):
     """
     TODO
     """
     df = prepare_df()
     fold_dfs = create_stratified_fold(df)
     train_loader, valid_loader = prepare_dataloaders(fold_dfs)
+    model, optimizer, scheduler = initiate_model_and_optimizer()
+    model, history = train_model(model, optimizer, scheduler, train_loader, valid_loader)
