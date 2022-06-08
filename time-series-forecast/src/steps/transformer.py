6d347f9844e2fb20876430c46490394c05ae65af
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-06-08 12:53:44 +0200

    Add description of feature engineering + add overall docstrings

--- time-series-forecast/src/steps/transformer.py
+++ time-series-forecast/src/steps/transformer.py
@@ -7,30 +7,43 @@ from sklearn.model_selection import train_test_split
 def transformer(data: pd.DataFrame) -> Output(
     X_train=np.ndarray, X_test=np.ndarray, y_train=np.ndarray, y_test=np.ndarray
 ):
+    """Feature engineering by transforming cardinal directions into 2-dimensional feature vectors.
+
+    First convert the cardinal directions (North, South, etc.) into degrees then calculate
+    cosine and sine values using a unit circle, these values make your vector [x1,x2] respectivaly
+    then to determine the magnitude of these new wind-direction-vectors, multiply them by the wind speed
+
+    Args:
+        data: DataFrame with training feature data and training target data
+
+    Returns:
+        X_train:np.array, X_test:np.array, y_train:np.array, y_test:np.array
+    """
     df = data.copy()
-    cardinal_directions = {'N': 0.0,
-                            'NNE': 0.0068591439603377154,
-                            'NE': 0.013700834628155488,
-                            'ENE': 0.0205599785884932,
-                            'E': 0.027419122548830915,
-                            'ESE': 1.9634954084936207,
-                            'SE': 2.356194490192345,
-                            'SSE': 2.748893571891069,
-                            'S': 3.141592653589793,
-                            'SSW': 3.5342917352885173,
-                            'SW': 3.9269908169872414,
-                            'WSW': 4.319689898685966,
-                            'W': 4.71238898038469,
-                            'WNW': 5.105088062083414,
-                            'NW': 5.497787143782138,
-                            'NNW': 5.8904862254808625}
+    cardinal_directions = {
+            'N':0,
+            'NNE':22.5,
+            'NE':45,
+            'ENE':67.5,
+            'E':90,
+            'ESE':112.5,
+            'SE':135,
+            'SSE':157.5,
+            'S':180,
+            'SSW':202.5,
+            'SW':225,
+            'WSW':247.5,
+            'W':270,
+            'WNW':292.5,
+            'NW':315,
+            'NNW':337.5}
 
     for direction in cardinal_directions:
-            df.loc[df["Direction"] == direction, "Direction"] = cardinal_directions[direction]
+        df.loc[df["Direction"] == direction, "Direction"] = cardinal_directions[direction]
     
     df['Direction'] = df['Direction'].astype(float)
-    df["v1"] =  df['Speed'] * np.cos(np.array(df["Direction"]))
-    df["v2"] =  df['Speed'] * np.sin(np.array(df["Direction"]))
+    df["v1"] =  df['Speed'] * np.cos(np.deg2rad(np.array(df["Direction"])))
+    df["v2"] =  df['Speed'] * np.sin(np.deg2rad(np.array(df["Direction"])))
     
     df = df.drop(['Direction', 'Speed'], axis=1)
 
