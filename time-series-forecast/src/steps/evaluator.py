a40a9fa03b25e263b87be9e34a28ea0e838badf2
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-06-08 12:52:08 +0200

    Add docstring for evaluator step + change logging Also add a try-except block to catch errors

--- time-series-forecast/src/steps/evaluator.py
+++ time-series-forecast/src/steps/evaluator.py
@@ -2,6 +2,7 @@ from zenml.steps import step, Output
 import numpy as np
 from sklearn.metrics import r2_score
 from sklearn.ensemble import RandomForestRegressor
+from zenml.logger import get_logger
 
 @step
 def evaluator(
@@ -9,9 +10,23 @@ def evaluator(
     y_test: np.ndarray,
     model: RandomForestRegressor,
 ) -> float:
-    
-    y_pred = model.predict(X_test)
+    """Evaluate a random forest using R2 score.
+
+    Args:
+        X_test: DataFrame with eval feature data.
+        y_test: DataFrame with eval target data.
+        model: Trained Random Forest Regressor. 
+
+    Returns:
+        float
+    """
+    logger = get_logger(__name__)
+    try:
+        y_pred = model.predict(X_test)
+    except:
+        logger.info("Error occured when predicting on test data")
+        
     score = r2_score(y_test,y_pred)
-    print(f'R2 score: {score}')
+    logger.info(f"R2 score: {score}")
 
     return score
