b09a31468dda7d7d914715fdf317ee719143902f
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-03-08 13:20:57 +0100

    update scripts

--- scripts/docstring.sh
+++ scripts/docstring.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-DOCSTRING_SRC=${1:-"src/zenml tests/harness"}
+DOCSTRING_SRC=${1:-"."}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,20 +3,19 @@ set -e
 set -x
 set -o pipefail
 
-SRC=${1:-"src/zenml tests examples"}
-SRC_NO_TESTS=${1:-"src/zenml tests/harness"}
-TESTS_EXAMPLES=${1:-"tests examples"}
+LINT_FILES=${1:-"."}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-ruff $SRC_NO_TESTS
+
+ruff $LINT_FILES
 # TODO: Fix docstrings in tests and examples and remove the `--extend-ignore D` flag
-ruff $TESTS_EXAMPLES --extend-ignore D
+ruff $LINT_FILES --extend-ignore D
 
 # autoflake replacement: checks for unused imports and variables
-ruff $SRC --select F401,F841 --exclude "__init__.py" --isolated
+ruff $LINT_FILES --select F401,F841 --exclude "__init__.py" --isolated
 
 black $SRC  --check
 
 # check type annotations
-mypy $SRC_NO_TESTS
+mypy $LINT_FILES
