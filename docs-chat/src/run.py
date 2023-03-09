f913045aef188a8f79e532b5a7b19c2e3dda773e
Felix Altenberger <felix@zenml.io>
2023-03-09 16:21:34 +0100

    Add function to train an index on the docs of each zenml version

--- docs-chat/src/run.py
+++ docs-chat/src/run.py
@@ -1,7 +1,11 @@
 import logging
+import os
+import shutil
 from typing import List
+from uuid import uuid4
 
 import faiss
+import git
 from langchain.docstore.document import Document
 from langchain.document_loaders import GitbookLoader
 from langchain.embeddings import OpenAIEmbeddings
@@ -9,6 +13,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS, VectorStore
 from llama_index import Document as LlamaDocument
 from llama_index import GPTFaissIndex, download_loader
+from tqdm import tqdm
 from zenml.integrations.registry import integration_registry
 from zenml.pipelines import pipeline
 from zenml.post_execution import get_pipeline
@@ -24,13 +29,16 @@ def docs_to_index_pipeline(document_loader, index_generator):
 class IndexGeneratorParameters(BaseParameters):
 
     docs_uri: str = "https://docs.zenml.io"
+    docs_base_url: str = "https://docs.zenml.io"
 
 
 @step
 def docs_loader(params: IndexGeneratorParameters) -> List[Document]:
-    # loader = GitbookLoader(params.docs_uri)
-    # page_data = loader.load()
-    loader = GitbookLoader(params.docs_uri, load_all_paths=True)
+    loader = GitbookLoader(
+        web_page=params.docs_uri,
+        base_url=params.docs_base_url,
+        load_all_paths=True,
+    )
     all_pages_data = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
@@ -74,7 +82,7 @@ def run_langchain():
         document_loader=docs_loader(),
         index_generator=index_generator(),
     )
-    pipeline.configure(enable_cache=False)
+    # pipeline.configure(enable_cache=False)
     pipeline.run()
 
 
@@ -83,7 +91,7 @@ def run_llama():
         document_loader=docs_loader(),
         index_generator=llama_index_generator(),
     )
-    pipeline.configure(enable_cache=False)
+    # pipeline.configure(enable_cache=False)
     pipeline.run()
 
 
@@ -98,7 +106,84 @@ def post_exec_llama_index():
     print(response)
 
 
+def get_zenml_versions():
+
+    # Clone zenml repo to random dir in CWD
+    random_repo_name = f"_{uuid4()}"
+    repo_dir = os.path.join(os.getcwd(), random_repo_name)
+    repo = git.Repo.clone_from(
+        url="https://github.com/zenml-io/zenml",
+        to_path=random_repo_name,
+    )
+
+    # Get all release versions
+    versions = []
+    for refs in repo.remote().refs:
+        branch_name = refs.name
+        if not branch_name.startswith("origin/release"):
+            continue
+        version = branch_name.split("/")[-1]
+        versions.append(version)
+
+    # Cleanup
+    shutil.rmtree(repo_dir)
+
+    return versions
+
+
+def _page_exists(url: str) -> bool:
+    import requests
+
+    r = requests.get(url)
+    if r.status_code == 200:
+        return True
+    return False
+
+
+def build_indices_for_zenml_versions(
+    versions: List[str], pipeline_name="zenml_docs_index_generation"
+):
+    @pipeline(name=pipeline_name)
+    def docs_to_index_pipeline(document_loader, index_generator):
+        documents = document_loader()
+        index_generator(documents)
+
+    for version in tqdm(versions):
+        base_url = "https://docs.zenml.io"
+        docs_url = f"https://docs.zenml.io/v/{version}"
+        if not _page_exists(docs_url):
+            print(f"Couldn't find docs page for zenml version '{version}'.")
+            continue
+        print(f"Building index for zenml docs of version '{version}'...")
+        pip = docs_to_index_pipeline(
+            document_loader=docs_loader(
+                params=IndexGeneratorParameters(
+                    docs_uri=docs_url, base_url=base_url
+                )
+            ),
+            index_generator=index_generator(),
+        )
+        run_name = (
+            f"{pipeline_name}" + "_{{{date}}}_{{{time}}}" + f"_{version}"
+        )
+        try:
+            pip.run(run_name=run_name)
+        except Exception as e:
+            print(f"Failed to build index for zenml version '{version}'.")
+            print(e)
+
+
+def main():
+    print("Fetching zenml versions...")
+    # versions = get_zenml_versions()  # all release versions
+    versions = ["0.10.0", "0.35.1"]
+    print(f"Found {len(versions)} versions.")
+    print("Building indices for zenml versions...")
+    build_indices_for_zenml_versions(versions)
+
+
 if __name__ == "__main__":
     logging.basicConfig(level="INFO")
-    run_llama()
-    post_exec_llama_index()
+    # run_llama()
+    # post_exec_llama_index()
+    main()
