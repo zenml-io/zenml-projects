bb43edaa6e18587f83525596021901f8252dc318
Felix Altenberger <felix@zenml.io>
2023-03-09 09:19:19 +0100

    Add llama-index-based index generator step

--- docs-chat/src/run.py
+++ docs-chat/src/run.py
@@ -1,13 +1,17 @@
 import logging
 from typing import List
 
+import faiss
 from langchain.docstore.document import Document
 from langchain.document_loaders import GitbookLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS, VectorStore
-from llama_index import download_loader
+from llama_index import Document as LlamaDocument
+from llama_index import GPTFaissIndex, download_loader
+from zenml.integrations.registry import integration_registry
 from zenml.pipelines import pipeline
+from zenml.post_execution import get_pipeline
 from zenml.steps import BaseParameters, step
 
 
@@ -46,6 +50,7 @@ def slack_loader(params: SlackLoaderParameters) -> List[Document]:
     SlackReader = download_loader("SlackReader")
     loader = SlackReader()
     documents = loader.load_data(channel_ids=params.channel_ids)
+    [d.to_langchain_format() for d in documents]
     return documents
 
 
@@ -56,11 +61,15 @@ def index_generator(documents: List[Document]) -> VectorStore:
     return vectorstore
 
 
-# def run_chat_bot():
-#     qa_chain = get_chain(vectorstore, question_handler, stream_handler)
+@step
+def llama_index_generator(documents: List[Document]) -> GPTFaissIndex:
+    documents = [LlamaDocument.from_langchain_format(d) for d in documents]
+    faiss_index = faiss.IndexFlatL2(1536)
+    index = GPTFaissIndex(documents, faiss_index=faiss_index)
+    return index
 
 
-def main():
+def run_langchain():
     pipeline = docs_to_index_pipeline(
         document_loader=docs_loader(),
         index_generator=index_generator(),
@@ -69,27 +78,20 @@ def main():
     pipeline.run()
 
 
+def run_llama():
+    pipeline = docs_to_index_pipeline(
+        document_loader=docs_loader(),
+        index_generator=llama_index_generator(),
+    )
+    pipeline.configure(enable_cache=False)
+    pipeline.run()
+
+
 def post_exec_llama_index():
-    import faiss
-    from llama_index import Document as LlamaDocument
-    from llama_index import GPTFaissIndex
-    from zenml.integrations.registry import integration_registry
-    from zenml.post_execution import get_pipeline
-
     integration_registry.activate_integrations()
-
     pipeline = get_pipeline("docs_to_index_pipeline")
-    runs = pipeline.runs
-    run = runs[0]  # TODO: -1 should be the latest run
-    documents = run.get_step("document_loader").output.read()
-    documents = [LlamaDocument.from_langchain_format(d) for d in documents]
-
-    # Creating a faiss index
-    d = 1536
-    faiss_index = faiss.IndexFlatL2(d)
-
-    # Load documents, build the GPTFaissIndex
-    index = GPTFaissIndex(documents, faiss_index=faiss_index)
+    last_run = pipeline.runs[0]
+    index = last_run.get_step("index_generator").output.read()
     response = index.query(
         "Are materializers loaded automatically during the post execution workflow?"
     )
@@ -98,4 +100,5 @@ def post_exec_llama_index():
 
 if __name__ == "__main__":
     logging.basicConfig(level="INFO")
-    main()
+    run_llama()
+    post_exec_llama_index()
