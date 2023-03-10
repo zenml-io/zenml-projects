d0c750168f553fff8ff123dd453079f9e67b1e4d
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-03-10 13:55:25 +0100

    remove imports

--- docs-chat/src/query_data.py
+++ docs-chat/src/query_data.py
@@ -8,7 +8,7 @@ from langchain.chains.chat_vector_db.prompts import (
 )
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI, HuggingFaceHub
+from langchain.llms import OpenAI
 from langchain.vectorstores.base import VectorStore
 
 
