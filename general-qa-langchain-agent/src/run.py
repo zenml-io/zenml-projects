dcb3f30f2dc30e56a578a8c3e523eaf3bfd5ab0b
Jayesh Sharma <wjayesh@outlook.com>
2023-06-13 19:39:58 +0530

    add weaviate settings as params

--- general-qa-langchain-agent/src/run.py
+++ general-qa-langchain-agent/src/run.py
@@ -14,10 +14,13 @@
 
 import logging
 
+from langchain.llms import HuggingFaceHub
+
 from pipelines.index_builder import docs_to_index_pipeline
-from steps.index_generator import index_generator
+from steps.index_generator import index_generator, IndexParameters
 from steps.url_scraper import UrlScraperParameters, url_scraper
 from steps.web_url_loader import web_url_loader
+from steps.agent_creator import agent_creator, AgentParameters
 
 
 def main():
@@ -26,6 +29,7 @@ def main():
     release_notes_url = (
         "https://github.com/zenml-io/zenml/blob/main/RELEASE_NOTES.md"
     )
+    weaviate_settings = {"url": "http://34.159.200.12"}
 
     slackbot_pipeline = docs_to_index_pipeline(
         url_scraper=url_scraper(
@@ -36,7 +40,19 @@ def main():
             )
         ),
         web_loader=web_url_loader(),
-        index_generator=index_generator(),
+        index_generator=index_generator(
+            config=IndexParameters(weaviate_settings=weaviate_settings)
+        ),
+        agent_creator=agent_creator(
+            config=AgentParameters(
+                llm=HuggingFaceHub(
+                    repo_id="google/flan-t5-xl",
+                    # huggingfacehub_api_token="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",
+                    model_kwargs={"temperature": 0, "max_length": 500},
+                ),
+                weaviate_settings=weaviate_settings,
+            )
+        ),
     )
 
     slackbot_pipeline.run()
