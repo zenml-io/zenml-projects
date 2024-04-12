5b624bbacd09968de0a02d796779b0477551347f
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-04-12 15:21:39 +0200

    update promotion

--- llm-peft-finetune/steps/promote.py
+++ llm-peft-finetune/steps/promote.py
@@ -16,6 +16,7 @@
 #
 
 from zenml import get_step_context, step
+from zenml.client import Client
 from zenml.logger import get_logger
 
 logger = get_logger(__name__)
@@ -23,18 +24,41 @@ logger = get_logger(__name__)
 
 @step
 def promote(metric: str = "rouge1", target_stage: str = "staging") -> None:
-    base_metrics = get_step_context().model.load_artifact(
-        "base_model_rouge_metrics"
-    )
-    ft_metrics = get_step_context().model.load_artifact(
-        "finetuned_model_rouge_metrics"
-    )
+    context_model = get_step_context().model
+    base_metrics = context_model.load_artifact("base_model_rouge_metrics")
+    ft_metrics = context_model.load_artifact("finetuned_model_rouge_metrics")
+    staging_metrics = None
+    try:
+        staging_model = Client().get_model_version(
+            context_model.name, target_stage
+        )
+        staging_metrics = staging_model.get_artifact(
+            "finetuned_model_rouge_metrics"
+        ).load()
+    except KeyError:
+        pass
 
-    logger.info(
-        f"`{metric}` to compare:\nbase={base_metrics[metric]*100:.2f}%\nfinetuned={ft_metrics[metric]*100:.2f}%"
+    msg = (
+        f"`{metric}` values to compare:\n"
+        f"base={base_metrics[metric]*100:.2f}%\n"
+        f"finetuned={ft_metrics[metric]*100:.2f}%"
     )
+    if staging_metrics:
+        msg += f"\nstaging={staging_metrics[metric]*100:.2f}%"
+    logger.info(msg)
+
     if base_metrics[metric] <= ft_metrics[metric]:
-        logger.info(f"Promoting model to `{target_stage}`")
-        get_step_context().model.set_stage(target_stage, True)
+        if staging_metrics is not None and (
+            staging_metrics[metric] > ft_metrics[metric]
+        ):
+            logger.info(
+                "Skipping promotion: model does not "
+                f"outperform the current model in `{target_stage}`."
+            )
+        else:
+            logger.info(f"Promoting model to `{target_stage}`")
+            get_step_context().model.set_stage(target_stage, True)
     else:
-        logger.info("Skipping promotion")
+        logger.info(
+            "Skipping promotion: model does not outperform the base model."
+        )
