dfd280f32caa371657da6a128a2db24bfb705e56
Alexej Penner <thealexejpenner@gmail.com>
2024-04-23 22:43:17 +0200

    Implemented data ingestion

---
+++ end-to-end-computer-vision/steps/download_from_hf.py
@@ -0,0 +1,84 @@
+# Apache Software License 2.0
+#
+# Copyright (c) ZenML GmbH 2024. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from typing import Dict, Any
+
+import fiftyone.zoo as foz
+from datasets import load_dataset
+from PIL import Image
+import os
+import json
+
+from zenml import step
+from zenml.logger import get_logger
+
+Image.MAX_IMAGE_PIXELS = None
+
+logger = get_logger(__name__)
+
+
+@step
+def download_dataset_from_hf() -> Dict[str, Any]:
+    dataset = load_dataset("datadrivenscience/ship-detection")
+    data = dataset['train']
+    output_dir = "/home/apenner/Desktop/ships/dataset"
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    all_images = {}
+
+    for i, d in enumerate(data):
+        print(d)
+        img = d['image']
+        img_path = f'{output_dir}/image_{i}.png'
+
+        print(f"Writing to {img_path}.")
+
+        img.save(img_path)
+
+        width, height = d['image'].size
+
+        results = []
+        for j, bbox in enumerate(d['objects']['bbox']):
+            x1, y1, x2, y2 = bbox
+            x = x1 / width
+            y = y1 / height
+            w = (x2 - x1) / width
+            h = (y2 - y1) / height
+            results.append(
+                {
+                    "original_width": width,
+                    "original_height": height,
+                    "image_rotation": 0,
+                    "value": {
+                        "x": x * 100,
+                        "y": y * 100,
+                        "width": w * 100,
+                        "height": h * 100,
+                        "rotation": 0,
+                        "rectanglelabels": [
+                            "ship"
+                        ]
+                    },
+                    "from_name": "label",
+                    "to_name": "image",
+                    "type": "rectanglelabels",
+                    "origin": "manual"
+                }
+            )
+
+        all_images[img_path] = results
+    return all_images
---
+++ end-to-end-computer-vision/steps/upload_to_label_studio.py
@@ -0,0 +1,57 @@
+# Apache Software License 2.0
+#
+# Copyright (c) ZenML GmbH 2024. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from typing import Dict, Any
+
+from zenml import step
+from zenml.client import Client
+
+
+@step
+def upload_labels_to_label_studio(
+        labels_dict: Dict[str, Any],
+        ls_project_id: int = 7
+):
+    """Uploads ground truth labels for images to label studio.
+
+    Args:
+        labels_dict: Dictionary mapping image filepath to list of bbox labels
+        ls_project_id: Project id of teh Label Studio project
+    """
+    annotator = Client().active_stack.annotator
+    from zenml.integrations.label_studio.annotators.label_studio_annotator import (
+        LabelStudioAnnotator,
+    )
+
+    if not isinstance(annotator, LabelStudioAnnotator):
+        raise TypeError(
+            "This step can only be used with the Label Studio annotator."
+        )
+
+    lsc = annotator._get_client()
+
+    project = lsc.get_project(ls_project_id)
+
+    tasks = project.get_tasks()
+
+    for task in tasks:
+        filename = task["storage_filename"]
+        print(filename)
+        project.create_annotation(
+            task["id"],
+            result=labels_dict[filename],
+            ground_truth=True
+        )
