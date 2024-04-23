689107a163ffa397982bb3c67ad46680d3068fe0
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-04-23 15:27:52 +0200

    fix inference import issues

--- end-to-end-computer-vision/pipelines/inference.py
+++ end-to-end-computer-vision/pipelines/inference.py
@@ -13,8 +13,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 import fiftyone as fo
 from zenml import pipeline
