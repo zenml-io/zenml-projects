b545fcbae2aa53994fd7e1e29d45a74e17129be8
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-05-08 19:20:41 +0200

    formatting

--- llm-complete-guide/notebooks/finetune_embeddings.py
+++ llm-complete-guide/notebooks/finetune_embeddings.py
@@ -1,20 +1,17 @@
-from sentence_transformers import SentenceTransformer
-from sentence_transformers import InputExample
-from datasets import load_dataset
-
-from torch.utils.data import DataLoader
-from sentence_transformers import losses
-from sklearn.metrics.pairwise import cosine_similarity
-from torch.utils.data import Dataset
-
 import wandb
+from datasets import load_dataset
+from sentence_transformers import InputExample, SentenceTransformer, losses
+from sklearn.metrics.pairwise import cosine_similarity
+from torch.utils.data import DataLoader, Dataset
 
 # Initialize wandb
 wandb.init(project="zenml_embeddings", entity="strickvl")
 
-model = SentenceTransformer('embedding-data/distilroberta-base-sentence-transformer')
+model = SentenceTransformer(
+    "embedding-data/distilroberta-base-sentence-transformer"
+)
 
-dataset_name = 'zenml/rag_qa_embedding_questions'
+dataset_name = "zenml/rag_qa_embedding_questions"
 train_dataset = load_dataset(dataset_name, split="train")
 test_dataset = load_dataset(dataset_name, split="test")
 
@@ -23,27 +20,33 @@ train_data = train_dataset
 n_examples = train_dataset.num_rows
 
 for i in range(n_examples):
-  example = train_data[i]
-  train_examples.append(InputExample(texts=[example['generated_questions'][0], example['page_content']]))
+    example = train_data[i]
+    train_examples.append(
+        InputExample(
+            texts=[example["generated_questions"][0], example["page_content"]]
+        )
+    )
 
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
 train_loss = losses.MultipleNegativesRankingLoss(model=model)
 num_epochs = 30
-warmup_steps = int(len(train_dataloader) * num_epochs * 0.1) #10% of train data
+warmup_steps = int(
+    len(train_dataloader) * num_epochs * 0.1
+)  # 10% of train data
 
 # Configure wandb
-wandb.config.update({
-    "num_epochs": num_epochs,
-    "warmup_steps": warmup_steps,
-    "batch_size": 32
-})
+wandb.config.update(
+    {"num_epochs": num_epochs, "warmup_steps": warmup_steps, "batch_size": 32}
+)
 
 # Track the training loss
 wandb.watch(model)
 
-model.fit(train_objectives=[(train_dataloader, train_loss)],
-          epochs=num_epochs,
-          warmup_steps=warmup_steps)
+model.fit(
+    train_objectives=[(train_dataloader, train_loss)],
+    epochs=num_epochs,
+    warmup_steps=warmup_steps,
+)
 
 # Evaluation
 test_examples = []
@@ -51,8 +54,12 @@ test_data = test_dataset
 n_test_examples = test_dataset.num_rows
 
 for i in range(n_test_examples):
-  example = test_data[i]
-  test_examples.append(InputExample(texts=[example['generated_questions'][0], example['page_content']]))
+    example = test_data[i]
+    test_examples.append(
+        InputExample(
+            texts=[example["generated_questions"][0], example["page_content"]]
+        )
+    )
 
 
 class InputExampleDataset(Dataset):
@@ -66,6 +73,7 @@ class InputExampleDataset(Dataset):
         example = self.examples[idx]
         return example.texts
 
+
 test_dataset = InputExampleDataset(test_examples)
 test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=32)
 
@@ -73,7 +81,9 @@ total_similarity = 0
 for batch in test_dataloader:
     question_embeddings = model.encode(batch[0])
     content_embeddings = model.encode(batch[1])
-    similarity_scores = cosine_similarity(question_embeddings, content_embeddings)
+    similarity_scores = cosine_similarity(
+        question_embeddings, content_embeddings
+    )
     total_similarity += similarity_scores.diagonal().sum()
 
 average_similarity = total_similarity / n_test_examples
