743ae9cb33e3c34f4a1f869526804125c3c85e06
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-06-26 18:38:46 +0200

    add data and get finetuning working

--- llm-complete-guide/notebooks/finetune_embeddings.py
+++ llm-complete-guide/notebooks/finetune_embeddings.py
@@ -7,9 +7,7 @@ from torch.utils.data import DataLoader, Dataset
 # Initialize wandb
 wandb.init(project="zenml_embeddings", entity="strickvl")
 
-model = SentenceTransformer(
-    "embedding-data/distilroberta-base-sentence-transformer"
-)
+model = SentenceTransformer("embedding-data/distilroberta-base-sentence-transformer")
 
 dataset_name = "zenml/rag_qa_embedding_questions"
 train_dataset = load_dataset(dataset_name, split="train")
@@ -22,17 +20,13 @@ n_examples = train_dataset.num_rows
 for i in range(n_examples):
     example = train_data[i]
     train_examples.append(
-        InputExample(
-            texts=[example["generated_questions"][0], example["page_content"]]
-        )
+        InputExample(texts=[example["generated_questions"][0], example["page_content"]])
     )
 
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
 train_loss = losses.MultipleNegativesRankingLoss(model=model)
 num_epochs = 30
-warmup_steps = int(
-    len(train_dataloader) * num_epochs * 0.1
-)  # 10% of train data
+warmup_steps = int(len(train_dataloader) * num_epochs * 0.1)  # 10% of train data
 
 # Configure wandb
 wandb.config.update(
@@ -56,9 +50,7 @@ n_test_examples = test_dataset.num_rows
 for i in range(n_test_examples):
     example = test_data[i]
     test_examples.append(
-        InputExample(
-            texts=[example["generated_questions"][0], example["page_content"]]
-        )
+        InputExample(texts=[example["generated_questions"][0], example["page_content"]])
     )
 
 
@@ -81,9 +73,7 @@ total_similarity = 0
 for batch in test_dataloader:
     question_embeddings = model.encode(batch[0])
     content_embeddings = model.encode(batch[1])
-    similarity_scores = cosine_similarity(
-        question_embeddings, content_embeddings
-    )
+    similarity_scores = cosine_similarity(question_embeddings, content_embeddings)
     total_similarity += similarity_scores.diagonal().sum()
 
 average_similarity = total_similarity / n_test_examples
