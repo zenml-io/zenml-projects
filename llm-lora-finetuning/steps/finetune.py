339543370dfba0cc04450a46124a23f78b52a8f9
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-07-23 23:10:17 +0200

    updates for llama 3.1

--- llm-lora-finetuning/steps/finetune.py
+++ llm-lora-finetuning/steps/finetune.py
@@ -15,12 +15,14 @@
 # limitations under the License.
 #
 
+import os
 from pathlib import Path
 
-import transformers
 from accelerate import Accelerator
 from datasets import load_from_disk
+import huggingface_hub
 from materializers.directory_materializer import DirectoryMaterializer
+import transformers
 from typing_extensions import Annotated
 from utils.callbacks import ZenMLCallback
 from utils.loaders import load_base_model
@@ -29,6 +31,7 @@ from zenml import step, ArtifactConfig
 from zenml.logger import get_logger
 from zenml.materializers import BuiltInMaterializer
 from zenml.utils.cuda_utils import cleanup_gpu_memory
+from zenml.client import Client
 
 logger = get_logger(__name__)
 
@@ -81,6 +84,16 @@ def finetune(
         The path to the finetuned model directory.
     """
     cleanup_gpu_memory(force=True)
+    
+    # authenticate with Hugging Face for gated repos
+    client = Client()
+
+    if not os.getenv("HF_TOKEN"):
+        try:
+            hf_token = client.get_secret("hf_token").secret_values['token']
+            huggingface_hub.login(token=hf_token)
+        except Exception as e:
+            logger.warning(f"Error authenticating with Hugging Face: {e}")
 
     ft_model_dir = Path("model_dir")
     dataset_dir = Path(dataset_dir)
