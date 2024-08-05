4d9f621bf52bb1b9506175cde12d3e25f5069173
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-08-05 17:53:47 +0200

    add finetuning step to pipeline

--- llm-complete-guide/pipelines/finetune_embeddings.py
+++ llm-complete-guide/pipelines/finetune_embeddings.py
@@ -1,4 +1,8 @@
-from steps.finetune_embeddings import evaluate_base_model, prepare_load_data
+from steps.finetune_embeddings import (
+    evaluate_base_model,
+    finetune,
+    prepare_load_data,
+)
 from zenml import pipeline
 
 
@@ -6,6 +10,7 @@ from zenml import pipeline
 def finetune_embeddings():
     data = prepare_load_data()
     evaluate_base_model(data)
+    finetune(data)
 
 
 if __name__ == "__main__":
