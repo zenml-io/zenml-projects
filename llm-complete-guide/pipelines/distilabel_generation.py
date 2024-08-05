afa24b09aad6fc9cb2830bb4edba393833c3eb22
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-08-05 11:20:17 +0200

    add final step to pipeline as active

--- llm-complete-guide/pipelines/distilabel_generation.py
+++ llm-complete-guide/pipelines/distilabel_generation.py
@@ -1,5 +1,6 @@
 from steps.distilabel_generate_queries import generate_synthetic_queries
 from steps.hf_dataset_loader import load_hf_dataset
+from steps.push_to_argilla import push_to_argilla
 from steps.push_to_hf import push_to_hf
 from zenml import pipeline
 
@@ -11,7 +12,7 @@ def generate_synthetic_data():
         train_dataset, test_dataset
     )
     push_to_hf(train_with_queries, test_with_queries)
-    # push_to_argilla(train_with_queries, test_with_queries)
+    push_to_argilla(train_with_queries, test_with_queries)
 
 
 if __name__ == "__main__":
