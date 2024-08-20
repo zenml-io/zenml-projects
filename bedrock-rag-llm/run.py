ce81e703209f3be6cc8e5ceb0179feed5d49d2b8
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-08-20 18:23:02 +0200

    working example

--- bedrock-rag-llm/run.py
+++ bedrock-rag-llm/run.py
@@ -1,56 +1,70 @@
 import json
 
-import boto3
 from botocore.exceptions import ClientError
 from rich import print
 from zenml import pipeline, step
+from zenml.client import Client
+from zenml.logger import get_logger
+from litellm import completion
+
+logger = get_logger(__name__)
+
+
+def generate_message(
+    bedrock_runtime, model_id, system_prompt, messages, max_tokens
+):
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": max_tokens,
+            "system": system_prompt,
+            "messages": messages,
+        }
+    )
+
+    response = bedrock_runtime.invoke_model(body=body, modelId=model_id)
+    return json.loads(response.get("body").read())
 
 
 @step
-def bedrock_basic_inference(model_id: str, prompt: str) -> str:
+def basic_inference(model_id: str, prompt: str) -> str:
     # Create an Amazon Bedrock Runtime client.
-    brt = boto3.client("bedrock-runtime")
+    zc = Client()
+    sc_client = zc.get_service_connector_client(
+        name_id_or_prefix="0b04bcae-efc9-4044-a1c2-b86281cb0820",
+        resource_type="aws-generic",
+    ).connect()
 
-    # Format the request payload using the model's native structure.
-    native_request = {
-        "inputText": prompt,
-        "textGenerationConfig": {
-            "maxTokenCount": 512,
-            "temperature": 0.5,
-            "topP": 0.9,
-        },
-    }
-
-    # Convert the native request to JSON.
-    request = json.dumps(native_request)
+    brt = sc_client.client("bedrock-runtime", region_name="us-east-1")
 
     try:
-        # Invoke the model with the request.
-        response = brt.invoke_model(modelId=model_id, body=request)
+        system_prompt = "You are a helpful assistant."
+        max_tokens = 1000
 
-    except (ClientError, Exception) as e:
-        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
-        exit(1)
+        # Prompt with user turn only.
+        user_message = {"role": "user", "content": prompt}
+        messages = [user_message]
 
-    # Decode the response body.
-    model_response = json.loads(response["body"].read())
+        response = generate_message(
+            brt, model_id, system_prompt, messages, max_tokens
+        )
+        logger.debug(response)
 
-    # Extract and print the response text.
-    response_text = model_response["results"][0]["outputText"]
-    print(response_text)
+        return response["content"][0]["text"]
 
-    return response_text
+    except ClientError as err:
+        message = err.response["Error"]["Message"]
+        logger.error("A client error occurred: %s", message)
+        print(f"A client error occured: {format(message)}")
 
 
 @pipeline
 def bedrock_basic_inference(model_id: str, prompt: str):
-    bedrock_basic_inference(model_id, prompt)
+    basic_inference(model_id, prompt)
 
 
 if __name__ == "__main__":
-    print(
-        bedrock_basic_inference(
-            model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
-            prompt="What is the capital of France?",
-        )
+    bedrock_basic_inference(
+        model_id="anthropic.claude-3-haiku-20240307-v1:0",
+        prompt="What is the capital of France?",
     )
