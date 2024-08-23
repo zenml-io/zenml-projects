5389fae548570ed51b40db6f72905d3a2a884dca
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-08-23 10:36:46 +0200

    update notebook

--- bedrock-rag-llm/constants.py
+++ bedrock-rag-llm/constants.py
@@ -1,1 +1,2 @@
 CLAUDE_3_HAIKU_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
+
--- bedrock-rag-llm/steps/create_and_sync_knowledge_base.py
+++ bedrock-rag-llm/steps/create_and_sync_knowledge_base.py
@@ -1,6 +1,27 @@
 from zenml import step
+from zenml.client import Client
 
 
 @step
-def create_and_sync_knowledge_base():
-    pass
+def create_and_sync_knowledge_base(
+    kb_name: str, kb_description: str, role_arn: str
+) -> str:
+    zc = Client()
+    sc_client = zc.get_service_connector_client(
+        name_id_or_prefix="0b04bcae-efc9-4044-a1c2-b86281cb0820",  # TODO: pull this out into config file
+        resource_type="aws-generic",
+    ).connect()
+    brc = sc_client.client("bedrock-agent")
+    kb_response = brc.create_knowledge_base(
+        name=kb_name,
+        description=kb_description,
+        roleArn="arn:aws:iam::339712793861:role/service-role/AmazonBedrockExecutionRoleForKnowledgeBase_96gjm",  # TODO: pull this out into config file
+        knowledgeBaseConfiguration={
+            "type": "VECTOR",
+            "vectorKnowledgeBaseConfiguration": {
+                "embeddingModel": {"name": "amazon.titan-embed-text-v1"}
+            },
+        },
+    )
+    kb_id = kb_response["knowledgeBase"]["knowledgeBaseId"]
+    return kb_id
