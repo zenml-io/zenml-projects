a4dd6b94396d3bbd55e79ee288042bd480b6d55c
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-09-06 13:22:41 +0200

    update inference CLI

--- bedrock-rag-llm/run.py
+++ bedrock-rag-llm/run.py
@@ -4,9 +4,11 @@ from constants import CLAUDE_3_HAIKU_MODEL_ID
 from pipelines.bedrock_basic_inference import bedrock_basic_inference
 from pipelines.bedrock_rag import bedrock_rag
 from zenml.logger import get_logger
+from zenml.client import Client
 
 logger = get_logger(__name__)
 
+sample_prompt = "What is the capital of France and what is the capital of Germany?"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -26,9 +28,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.pipeline == "inference":
+        print("Asking model: ", sample_prompt)
         bedrock_basic_inference(
             model_id=args.model_id,
-            prompt="What is the capital of France and what is the capital of Germany?",
+            prompt=sample_prompt,
         )
+        zc = Client()
+        output = zc.get_pipeline("bedrock_basic_inference").last_successful_run.steps['basic_inference'].outputs['output'].load()
+        print(f"Answer: '{output}'")
     elif args.pipeline == "rag":
         bedrock_rag()
