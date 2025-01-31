a7105fbb685e7070237732f68ee940da76281b33
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-01-31 15:34:54 +0100

    Update ZenML model version and fix vector store metadata access

--- llm-complete-guide/utils/llm_utils.py
+++ llm-complete-guide/utils/llm_utils.py
@@ -439,9 +439,9 @@ def find_vectorstore_name() -> str:
     """
     from zenml.client import Client
     client = Client()
-    model = client.get_model_version(ZENML_CHATBOT_MODEL, model_version_name_or_number_or_id="v0.68.1-dev")
+    model = client.get_model_version(ZENML_CHATBOT_MODEL, model_version_name_or_number_or_id="0.71.0-dev")
 
-    return model.run_metadata["vector_store"].value["name"]
+    return model.run_metadata["vector_store"]["name"]
 
 
 def rerank_documents(
