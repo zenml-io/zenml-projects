efeec17fa3355e7ec61e454d086d54c07b60f71d
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-03-27 09:51:20 +0100

    Add basic zenml pipeline

--- sonicscribe/run.py
+++ sonicscribe/run.py
@@ -1,6 +1,15 @@
-def main():
+from zenml import step, pipeline
+
+
+@step
+def upload_audio_file(folder_path: str):
     print("Hello from sonicscribe!")
 
 
+@pipeline
+def my_pipeline():
+    upload_audio_file(folder_path="audio_files")
+
+
 if __name__ == "__main__":
-    main()
+    my_pipeline()
