625156ae58a2426e90bd38df3f9fae559fc04d92
marwan37 <marwan.ext@zenml.io>
2025-03-29 13:33:45 -0500

    Add detailed metrics to streamlit UI app

--- omni-reader/app.py
+++ omni-reader/app.py
@@ -1,3 +1,19 @@
+# Apache Software License 2.0
+#
+# Copyright (c) ZenML GmbH 2025. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """This is the Streamlit UI for the ZenML OCR Extraction workflow."""
 
 import base64
@@ -84,8 +100,8 @@ with st.sidebar:
     st.markdown("---")
     st.caption("Powered by Ollama + MistralAI")
 
-gemma_logo = base64.b64encode(open("./assets/gemma3.svg", "rb").read()).decode()
-mistral_logo = base64.b64encode(open("./assets/mistral-2.svg", "rb").read()).decode()
+gemma_logo = base64.b64encode(open("./assets/logos/gemma.svg", "rb").read()).decode()
+mistral_logo = base64.b64encode(open("./assets/logos/mistral.svg", "rb").read()).decode()
 
 # Main header with optional logo
 if model_choice == "Gemma-3":
