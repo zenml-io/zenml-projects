4882760138a77812ebd3f130e914759da89cb7b9
marwan37 <marwan.ext@zenml.io>
2025-03-29 11:44:56 -0500

    remove integrations utils: remove mlflow, and docker for now

--- omni-reader/utils/encode_image.py
+++ omni-reader/utils/encode_image.py
@@ -1,3 +1,19 @@
+# Apache Software License 2.0
+#
+# Copyright (c) ZenML GmbH 2025. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """This module contains utility functions for encoding images to base64 strings."""
 
 import base64
@@ -53,11 +69,7 @@ def encode_image(image: Image.Image) -> tuple[str, str]:
         image_base64 = encode_image_from_path(image)
     else:
         img_format = image.format or "JPEG"
-        content_type = (
-            f"image/{img_format.lower()}" if img_format else "image/jpeg"
-        )
-        image_base64 = encode_pil_image(
-            image, format=img_format if img_format else "JPEG"
-        )
+        content_type = f"image/{img_format.lower()}" if img_format else "image/jpeg"
+        image_base64 = encode_pil_image(image, format=img_format if img_format else "JPEG")
 
     return content_type, image_base64
