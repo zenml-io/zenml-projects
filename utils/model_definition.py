b73013ffe6434b30f15b5aeefec7c74ec54cdd5e
marwan37 <marwan.ext@zenml.io>
2025-05-11 08:15:03 -0600

    add model definition to utils

---
+++ utils/model_definition.py
@@ -0,0 +1,13 @@
+from pydantic import ConfigDict
+from zenml import Model
+
+model_definition = Model(
+    name="credit_scoring_model",
+    version="gb",
+    license="Apache 2.0",
+    description="A credit scoring model",
+    tags=["credit_scoring", "classifier"],
+    audience="ZenML users",
+    use_cases="EU AI Act compliance, risk assessment",
+    model_config=ConfigDict(arbitrary_types_allowed=True),
+)
